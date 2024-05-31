4feb21dfcf6c31165795c92122a5f692e87cb321
Andrea Lizeth Lopez Chavez <lopezchavezandrealizeth@gmail.com>
2024-05-30 22:42:11 -0600

    api usuario admin

--- front-end/vistas/administrador/src/scripts/formularioUsuarioApi.js
+++ front-end/vistas/administrador/src/scripts/formularioUsuarioApi.js
@@ -1,4 +1,3 @@
-
 function actualizarTabla() {
     const url = "http://localhost:8081/api/crea_chet/usuarios";
 
@@ -32,7 +31,8 @@ function actualizarTabla() {
                 .catch (error => {
         console.error(error)
     })
-};
+}
+
 //eliminar id boton tabla
 // Función para eliminar usuario por ID
 function eliminarUsuario(id) {
@@ -59,61 +59,145 @@ function eliminarUsuario(id) {
         console.error(error);
     });
 }
+
+//Boton Agregar usuario
 //Boton Agregar usuario
 function agregarUsuario() {
-  // Obtener los valores de los campos de entrada
-  const idU = document.getElementById("idUser").value;
-  const nombreU = document.getElementById("nameUser").value;
-  const aPaterno = document.getElementById("aPaternoUser").value;
-  const aMaterno = document.getElementById("aMaternoUser").value;
-  const email = document.getElementById("emailUser").value;
-  const password = document.getElementById("passwordUser").value;
-  const telefono = document.getElementById("telUser").value;
-  const fotoPerfil = document.getElementById("fotoUser").value;
-  const rol = document.querySelector('input[name="rol"]:checked').value;
+    // Obtener los valores de los campos de entrada
+    const idU = document.getElementById("idUser").value;
+    const nombreU = document.getElementById("nameUser").value;
+    const aPaterno = document.getElementById("aPaternoUser").value;
+    const aMaterno = document.getElementById("aMaternoUser").value;
+    const email = document.getElementById("emailUser").value;
+    const password = document.getElementById("passwordUser").value;
+    const telefono = document.getElementById("telUser").value;
+    const fotoPerfil = document.getElementById("fotoUser").value;
+    const rol = document.querySelector('input[name="rol"]:checked').value;
+  
+     // Crear un objeto con los datos del usuario
+     const usuario = {
+      id: idU,
+      name: nombreU,
+      lastName: aPaterno,
+      middleName: aMaterno,
+      email: email,
+      password: password,
+      phoneNumber: telefono,
+      profileImage: fotoPerfil,
+      role: rol
+  };
+  //Método post 
+  const url="http://localhost:8081/api/crea_chet/usuarios";
+     
+      fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(usuario)
+      })
+          .then(response => {
+              return response.json();
+          })
+          .then(data => {
+              console.log('Guardado', data)
+              actualizarTabla();
+              $('#exampleModal').modal('hide');
+          })
+          .catch(error => {
+              console.error(error);
+          })
+  }
 
-   // Crear un objeto con los datos del usuario
-   const usuario = {
-    id: idU,
-    name: nombreU,
-    lastName: aPaterno,
-    middleName: aMaterno,
-    email: email,
-    password: password,
-    phoneNumber: telefono,
-    profileImage: fotoPerfil,
-    role: rol
-};
-//Método post 
-const url="http://localhost:8081/api/crea_chet/usuarios";
-   
+
+////////////////////////////////////////////////////////////////
+function actualizarUsuario() {
+
+    // Obtener los valores actualizados de los campos de entrada
+    const nuevoIdU = document.getElementById("idUser1").value;
+    const nuevoNombreU = document.getElementById("nameUser1").value;
+    const nuevoAPaterno = document.getElementById("aPaternoUser1").value;
+    const nuevoAMaterno = document.getElementById("aMaternoUser1").value;
+    const nuevoEmail = document.getElementById("emailUser1").value;
+    const nuevoPassword = document.getElementById("passwordUser1").value;
+    const nuevoTelUser = document.getElementById("telUser1").value; // Obtener el teléfono
+    const nuevaFotoUser = document.getElementById("fotoUser1").value; // Obtener la foto de perfil
+    const nuevoRol = document.querySelector('input[name="rol"]:checked').value; // Obtener el rol
+    
+    const usuario={
+        id: nuevoIdU,
+        name: nuevoNombreU,
+        lastName: nuevoAPaterno,
+        middleName: nuevoAMaterno,
+        email: nuevoEmail,
+        password:  nuevoPassword,
+        phoneNumber: nuevoTelUser,
+        profileImage: nuevaFotoUser,
+        role: nuevoRol
+    }
+
+
+    const url = `http://localhost:8081/api/crea_chet/usuarios/${nuevoIdU}`;
     fetch(url, {
-        method: 'POST',
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(usuario)
+
     })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log('Guardado', data)
+    .then(response => {
+        if (response.ok) {
+           
+            
             actualizarTabla();
-            $('#exampleModal').modal('hide');
-        })
-        .catch(error => {
-            console.error(error);
-        })
+           
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
-    
-   
 
+    $('#exampleModal1').modal('hide');
 }
 
 
+function abrirModalActualizar(id) {
 
-//cargar la pagina 
+    $('#exampleModal1').modal('show');
+    // Cargar los datos del usuario en el formulario
+    cargarDatosEnFormulario(id);
+    $('#exampleModal1').modal('hide');
+}
+
+
+function cargarDatosEnFormulario(id) {
+    const url = `http://localhost:8081/api/crea_chet/usuarios/${id}`;
+    fetch(url)
+        .then(response => response.json())
+
+        .then(data =>{
+            document.getElementById("idUser1").value = data.id;
+            document.getElementById("nameUser1").value = data.name;
+            document.getElementById("aPaternoUser1").value = data.lastName;
+            document.getElementById("aMaternoUser1").value = data.middleName;
+            document.getElementById("emailUser1").value = data.email;
+            document.getElementById("passwordUser1").value = data.password;
+            document.getElementById("telUser1").value = data.phoneNumber;
+            document.getElementById("fotoUser1").value = data.profileImage;
+            if (data.role === "CLIENTE") {
+                document.getElementById("cliente1").checked = true;
+            } else if (data.role === "ADMINISTRADOR") {
+                document.getElementById("administrador1").checked = true;
+            }
+        } )
+                .catch (error => {
+        console.error(error)
+    })
+}
+
+
 window.onload = () => {
     actualizarTabla();
 };
